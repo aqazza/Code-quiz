20da98bad64fcf350bb5bde290407bd56596b746
Anas Qazza <anasqazza@gmail.com>
2022-10-25 18:11:15 -0700

    Refunctioned JS

--- script.js
+++ script.js
@@ -1,41 +1,62 @@
 const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
-const questionElement= document.getElementById('question')
+const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
+const timerElement = document.getElementById('timer')
 
-let randomQuestions, currentQuestionIndex
-
-startButton.addEventListener('click', beginGame)
+let shuffledQuestions, currentQuestionIndex
+let timeLeft = 10;
+let timerReference
+startButton.addEventListener('click', startGame)
 nextButton.addEventListener('click', () => {
     currentQuestionIndex++
-    NextQuestion()
+    setNextQuestion()
 })
 
-function beginGame() {
-// console.log('Starting')
-startButton.classList.add('hide')
-randomQuestions = questions.sort(() => Math.random() - .5)
-currentQuestionIndex = 0
-questionContainerElement.classList.remove('hide')
-NextQuestion()
+function startGame() {
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 
-function NextQuestion() {
-    resetState()
-showQuestion(randomQuestions[currentQuestionIndex])
+function countdown() {
+    timeLeft--;
+    timerElement.textContent = timeLeft;
+    if(timeLeft === 0){
+        clearInterval(timerReference);
+        // stop the timer, go to next question
+        currentQuestionIndex++
+        setNextQuestion()
+    }
+}
+
+function startTimer() {
+    timeLeft = 10;
+    timerElement.textContent = timeLeft;
+    clearInterval(timerReference);
+    //store the id of the timer as a var
+    timerReference = setInterval(countdown, 1000);
+}
+
+function setNextQuestion() {
+    resetState();
+    startTimer();
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
     questionElement.innerText = question.question
-    questions.answers.forEach(answer => {
+    question.answers.forEach(answer => {
         const button = document.createElement('button')
         button.innerText = answer.text
         button.classList.add('btn')
         if (answer.correct) {
             button.dataset.correct = answer.correct
         }
-        button.addEventListener('click', chooseAnswer)
+        button.addEventListener('click', selectAnswer)
         answerButtonsElement.appendChild(button)
     })
 }
@@ -45,16 +66,17 @@ function resetState() {
     nextButton.classList.add('hide')
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-        }
     }
-function chooseAnswer(e) {
-    const chosenButton = e.target
-    const correct = chosenButton.dataset.correct
+}
+
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
     setStatusClass(document.body, correct)
     Array.from(answerButtonsElement.children).forEach(button => {
         setStatusClass(button, button.dataset.correct)
     })
-    if (randomQuestions.length > currentQuestionIndex + 1) {
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
         nextButton.classList.remove('hide')
     } else {
         startButton.innerText = 'Restart'
@@ -62,14 +84,56 @@ function chooseAnswer(e) {
     }
 }
 
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+
 const questions = [
     {
-     questions: "What is the greatest soccer team of all time?"  ,
-     answers: [
-        { text: "FC Barcelona", correct: false },
-        { text: "Real Madrid", correct: true },
-        { text: "Arsenal FC", correct: false },
-        { text: "Bayern Munich", correct: false },
-     ]   
+        question: 'Who is a current professor at UCI Coding Bootcamp?',
+        answers: [
+            { text: 'Chad Tao', correct: true },
+            { text: 'Albert Einstein', correct: false },
+            { text: 'John McEnroe', correct: false },
+            { text: 'Serena Williams', correct: false },
+        ]
+    },
+    {
+        question: 'What is the best programming language?',
+        answers: [
+            { text: 'CSS', correct: false },
+            { text: 'Javascript', correct: false },
+            { text: 'HTML', correct: false },
+            { text: 'All of the Above', correct: true }
+        ]
+    },
+    {
+        question: 'Which of the following is a fruit?',
+        answers: [
+            { text: 'Tomato', correct: false },
+            { text: 'Orange', correct: true },
+            { text: 'Potato', correct: false },
+            { text: 'Chicken', correct: false }
+        ]
+    },
+    {
+        question: 'Who is the greatest basketball player of all time?',
+        answers: [
+            { text: 'Michael Jordan', correct: false },
+            { text: 'Kobe Bryant', correct: true },
+            { text: 'Lebron James', correct: false },
+            { text: 'Kareem-Abdul Jabbar', correct: false }
+        ]
     }
 ]
